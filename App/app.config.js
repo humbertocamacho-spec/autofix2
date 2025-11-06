d0991029efdf2ef5eb310a11f10afb3eaa598ee7
unknown <carlos.gutierrez@puntosingular.com>
2025-11-06 13:11:18 -0600

    Correciones

--- App/app.config.js
+++ App/app.config.js
@@ -5,8 +5,41 @@ export default {
     name: 'AutoFix',
     slug: 'autofix',
     version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'autofix',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: 'com.anonymous.autofix',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.jpg',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+          dark: {
+            backgroundColor: '#000000',
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
     extra: {
-      API_URL: process.env.API_URL,
+      API_URL: process.env.API_URL || 'https://default-api-url.com',
+      eas: {
+        projectId: '',
+      },
     },
   },
 };
