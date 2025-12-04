5541341004a623507abf5f2406f6472f1cb04e6a
unknown <carlos.gutierrez@puntosingular.com>
2025-12-04 15:08:43 -0600

    Imagen Register

--- Web/src/pages/auth/RegisterPage.tsx
+++ Web/src/pages/auth/RegisterPage.tsx
@@ -3,13 +3,11 @@ import RegisterForm from "../../components/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage: "url('/images/login-bg-1.jpg')",
-      }}
-    >
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center">
       <div className="w-full max-w-xl bg-white rounded-2xl shadow-2xl p-10">
+        <div className="text-center mb-3">
+          <img src="../../assets/images/Logo.jpg" alt="" className="w-60 h-auto object-contain select-none mx-auto my-auto block" />
+        </div>
         <h4 className="text-2xl font-semibold mb-2 text-gray-900 text-center">
           Create your account
         </h4>
