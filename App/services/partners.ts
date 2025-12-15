7468a949a493788ccec8557d9451898312113442
unknown <carlos.gutierrez@puntosingular.com>
2025-12-15 11:38:30 -0600

    Filtrado Partners

--- App/services/partners.ts
+++ App/services/partners.ts
@@ -1,20 +1,31 @@
 import { API_URL } from '../config/env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export async function getPartners() {
-    try {
-        const res = await fetch(`${API_URL}/api/partners`);
+  try {
+    const token = await AsyncStorage.getItem('token');
 
-        if (!res.ok) {
-            console.error(`Error HTTP: ${res.status} al obtener partners`);
-            return [];
-        }
-
-        const data = await res.json();
-        return data;
-
-    } catch (error) {
-        console.error("Error en getPartners:", error);
-        return [];
+    if (!token) {
+      console.log('No hay token guardado');
+      return [];
     }
 
+    const res = await fetch(`${API_URL}/api/partners`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      const text = await res.text();
+      console.error('Error backend:', text);
+      return [];
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error('Error en getPartners:', error);
+    return [];
+  }
 }
