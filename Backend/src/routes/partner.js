3b95be32b0bc8ad527c6c2acda4ea95eaae0d3a9
unknown <carlos.gutierrez@puntosingular.com>
2025-12-15 10:49:16 -0600

    Auth 1

--- Backend/src/routes/partner.js
+++ Backend/src/routes/partner.js
@@ -19,12 +19,23 @@ router.get("/select", authMiddleware, async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
+  console.log("USER DESDE TOKEN:", req.user);
   try {
-    const [rows] = await db.query(`
+    const { user_id, role_id } = req.user;
+
+    const [roleRow] = await db.query(
+      "SELECT name FROM roles WHERE id = ?",
+      [role_id]
+    );
+
+    const roleName = roleRow[0]?.name?.toLowerCase();
+
+    let sql = `
       SELECT 
         p.id,
         p.name,
+        p.user_id,
         p.whatsapp,
         p.phone,
         p.location,
@@ -37,9 +48,18 @@ router.get("/", async (req, res) => {
         p.priority
       FROM partners p
       JOIN users u ON p.user_id = u.id
-      ORDER BY p.priority ASC, p.name ASC;
-    `);
+    `;
 
+    const params = [];
+
+    if (roleName === "partner") {
+      sql += " WHERE p.user_id = ?";
+      params.push(user_id);
+    }
+
+    sql += " ORDER BY p.priority ASC, p.name ASC";
+
+    const [rows] = await db.query(sql, params);
     res.json(rows);
   } catch (error) {
     console.error("Error al obtener los partners:", error);
