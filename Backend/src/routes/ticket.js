c81090047bd8920e68204e26b228251c52dd69db
unknown <carlos.gutierrez@puntosingular.com>
2025-12-17 10:51:11 -0600

    Ticket por Rol

--- Backend/src/routes/ticket.js
+++ Backend/src/routes/ticket.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "../config/db.js";
-import { authMiddleware } from "../middleware/auth.js";
+import { authMiddleware } from "./auth.js";
 
 const router = express.Router();
 
