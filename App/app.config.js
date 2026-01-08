9008eddb708f98dd4f69ef4be83321939fdc812f
unknown <carlos.gutierrez@puntosingular.com>
2026-01-07 19:46:17 -0600

    versioncode

--- App/app.config.js
+++ App/app.config.js
@@ -29,7 +29,7 @@ export default {
           "apiKey": process.env.GOOGLE_API_KEY,
         }
       },
-      versionCode: 2,
+      versionCode: 4,
       permissions: [
         "ACCESS_FINE_LOCATION",
         "ACCESS_COARSE_LOCATION",
