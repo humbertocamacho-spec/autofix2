8a0d7ace7f6268f620d5ba8a8f7dc2be356d75ba
unknown <carlos.gutierrez@puntosingular.com>
2026-01-15 16:58:53 -0600

    auth car

--- Backend/src/routes/car.js
+++ Backend/src/routes/car.js
@@ -1,112 +1,113 @@
 import express from "express";
 import db from "../config/db.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Endpoint to get all cars
-router.get("/", async (req, res) => {
-    try {
-        const [rows] = await db.query("SELECT * FROM cars");
-        res.json(rows);
-    } catch (error) {
-        console.error("Error al obtener cars:", error);
-        res.status(500).json({ message: "Error al obtener vehículos" });
-    }
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+      const [rows] = await db.query("SELECT * FROM cars");
+      res.json(rows);
+  } catch (error) {
+      console.error("Error al obtener cars:", error);
+      res.status(500).json({ message: "Error al obtener vehículos" });
+  }
 });
 
 // Endpoint to create a car
-router.post("/", async (req, res) => {
-    const { name, car_brand_id, model, year, type, plate, client_id } = req.body;
+router.post("/", authMiddleware, async (req, res) => {
+  const { name, car_brand_id, model, year, type, plate, client_id } = req.body;
 
-    if (!client_id) {
-        return res.status(400).json({ message: "Falta client_id" });
-    }
+  if (!client_id) {
+      return res.status(400).json({ message: "Falta client_id" });
+  }
 
-    if (!name || !car_brand_id || !model || !year || !plate) {
-        return res.status(400).json({ message: "Faltan campos obligatorios" });
-    }
+  if (!name || !car_brand_id || !model || !year || !plate) {
+      return res.status(400).json({ message: "Faltan campos obligatorios" });
+  }
 
-    if (!/^\d{4}$/.test(year)) {
-        return res.status(400).json({ message: "El año debe tener 4 dígitos numéricos" });
-    }
+  if (!/^\d{4}$/.test(year)) {
+      return res.status(400).json({ message: "El año debe tener 4 dígitos numéricos" });
+  }
 
-    const [existPlate] = await db.query(
-        "SELECT id FROM cars WHERE plate = ?",
-        [plate]
-    );
+  const [existPlate] = await db.query(
+      "SELECT id FROM cars WHERE plate = ?",
+      [plate]
+  );
 
-    if (existPlate.length > 0) {
-        return res.status(409).json({ message: "La placa ya está registrada en otro vehículo" });
-    }
+  if (existPlate.length > 0) {
+      return res.status(409).json({ message: "La placa ya está registrada en otro vehículo" });
+  }
 
-    try {
-        const [result] = await db.query(
-            "INSERT INTO cars (name, car_brand_id, model, year, type, plate) VALUES (?, ?, ?, ?, ?, ?)",
-            [name, car_brand_id, model, year, type, plate]
-        );
+  try {
+      const [result] = await db.query(
+          "INSERT INTO cars (name, car_brand_id, model, year, type, plate) VALUES (?, ?, ?, ?, ?, ?)",
+          [name, car_brand_id, model, year, type, plate]
+      );
 
-        const carId = result.insertId;
-        await db.query(
-            "INSERT INTO cars_clients (client_id, car_id) VALUES (?, ?)",
-            [client_id, carId]
-        );
+      const carId = result.insertId;
+      await db.query(
+          "INSERT INTO cars_clients (client_id, car_id) VALUES (?, ?)",
+          [client_id, carId]
+      );
 
-        res.json({ ok: true, message: "Vehículo creado", car_id: carId });
+      res.json({ ok: true, message: "Vehículo creado", car_id: carId });
 
-    } catch (error) {
-        console.error("Error al crear vehículo:", error);
-        res.status(500).json({ message: "Error al crear vehículo" });
-    }
+  } catch (error) {
+      console.error("Error al crear vehículo:", error);
+      res.status(500).json({ message: "Error al crear vehículo" });
+  }
 });
 
 // Endpoint to get a car by id
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { name, car_brand_id, model, year, type, plate } = req.body;
+router.put("/:id",authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const { name, car_brand_id, model, year, type, plate } = req.body;
 
-    if (!/^\d{4}$/.test(year)) {
-        return res.status(400).json({ message: "El año debe tener 4 dígitos numéricos" });
-    }
+  if (!/^\d{4}$/.test(year)) {
+      return res.status(400).json({ message: "El año debe tener 4 dígitos numéricos" });
+  }
 
-    const [existPlate] = await db.query(
-        "SELECT id FROM cars WHERE plate = ? AND id != ?",
-        [plate, id]
-    );
+  const [existPlate] = await db.query(
+      "SELECT id FROM cars WHERE plate = ? AND id != ?",
+      [plate, id]
+  );
 
-    if (existPlate.length > 0) {
-        return res.status(409).json({ message: "Otra unidad ya usa esta placa" });
-    }
+  if (existPlate.length > 0) {
+      return res.status(409).json({ message: "Otra unidad ya usa esta placa" });
+  }
 
-    try {
-        await db.query(
-            "UPDATE cars SET name=?, car_brand_id=?, model=?, year=?, type=?, plate=? WHERE id=?",
-            [name, car_brand_id, model, year, type, plate, id]
-        );
+  try {
+      await db.query(
+          "UPDATE cars SET name=?, car_brand_id=?, model=?, year=?, type=?, plate=? WHERE id=?",
+          [name, car_brand_id, model, year, type, plate, id]
+      );
 
-        res.json({ ok: true, message: "Vehículo actualizado" });
+      res.json({ ok: true, message: "Vehículo actualizado" });
 
-    } catch (error) {
-        console.error("Error al actualizar vehículo:", error);
-        res.status(500).json({ message: "Error al actualizar vehículo" });
-    }
+  } catch (error) {
+      console.error("Error al actualizar vehículo:", error);
+      res.status(500).json({ message: "Error al actualizar vehículo" });
+  }
 });
 
 // Endpoint to delete a car by id
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
+router.delete("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params;
 
-    try {
+  try {
 
-        await db.query("DELETE FROM cars_clients WHERE car_id = ?", [id]);
+      await db.query("DELETE FROM cars_clients WHERE car_id = ?", [id]);
 
-        await db.query("DELETE FROM cars WHERE id = ?", [id]);
+      await db.query("DELETE FROM cars WHERE id = ?", [id]);
 
-        res.json({ ok: true, message: "Vehículo eliminado" });
+      res.json({ ok: true, message: "Vehículo eliminado" });
 
-    } catch (error) {
-        console.error("Error al eliminar vehículo:", error);
-        res.status(500).json({ message: "Error al eliminar vehículo" });
-    }
+  } catch (error) {
+      console.error("Error al eliminar vehículo:", error);
+      res.status(500).json({ message: "Error al eliminar vehículo" });
+  }
 });
 
 export default router;
