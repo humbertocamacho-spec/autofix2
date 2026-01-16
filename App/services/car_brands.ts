985364456ff047a8e6be836463b7458bd4957d47
unknown <carlos.gutierrez@puntosingular.com>
2026-01-16 14:11:05 -0600

    auth brands

--- App/services/car_brands.ts
+++ App/services/car_brands.ts
@@ -1,5 +1,5 @@
-import { API_URL } from "../config/env";
-import { authFetch } from "../utils/authFetch";
+import { API_URL } from "@/config/env";
+import { authFetch } from "@/utils/authFetch";
 
 export async function getCarBrands() {
   try {
