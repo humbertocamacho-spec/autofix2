0d3c27d91232c691a19681478321eaa74d1e0c18
unknown <carlos.gutierrez@puntosingular.com>
2026-01-18 16:38:42 -0600

    Corrections

--- Backend/knexfile.js
+++ Backend/knexfile.js
@@ -4,9 +4,9 @@ export default {
     connection: {
       host: 'hopper.proxy.rlwy.net', // host público de Railway
       port: 26627,                   // puerto público de Railway
-      user: 'root',                  // tu usuario
-      password: 'XxVkKtWmhxRXclsruWISzhslwGgWoKQd', // tu contraseña
-      database: 'railway'            // tu base de datos
+      user: 'root',                  // usuario
+      password: 'XxVkKtWmhxRXclsruWISzhslwGgWoKQd', // contraseña
+      database: 'railway'            // base de datos
     },
     migrations: { directory: './database/migrations' },
     seeds: { directory: './database/seeds' },
@@ -27,11 +27,11 @@ export default {
   production: {
     client: 'mysql2',
     connection: {
-      host: 'hopper.proxy.rlwy.net',
-      port: 26627,
-      user: 'root',
-      password: 'XxVkKtWmhxRXclsruWISzhslwGgWoKQd',
-      database: 'railway'
+      host: 'localhost',
+      port: 3000,
+      user: 'autofix',
+      password: 'lsPLMne8Hnbn2',
+      database: 'autofix'
     },
     pool: { min: 2, max: 10 },
     migrations: { tableName: 'knex_migrations', directory: './database/migrations' },
