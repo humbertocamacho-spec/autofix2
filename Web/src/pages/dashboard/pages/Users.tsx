b0d08d0f26d35685481f9a95171c7c4a555ff0b1
unknown <carlos.gutierrez@puntosingular.com>
2026-01-18 20:42:47 -0600

    delete create users

--- Web/src/pages/dashboard/pages/Users.tsx
+++ Web/src/pages/dashboard/pages/Users.tsx
@@ -54,60 +54,10 @@ export default function UsersTable() {
     }
   };
 
-  // Open create modal
-  const handleOpenCreate = () => {
-    setCurrentUser({
-      name: "",
-      email: "",
-      phone: "",
-      address: "",
-      role_id: 3,
-      gender_id: null,
-      photo_url: null,
-    } as User);
-
-    setOpenEdit(true);
-  };
-
   // Open edit modal
   const handleOpenEdit = (user: User) => {
     setCurrentUser(user);
     setOpenEdit(true);
-  };
-
-  const handleSaveUser = async () => {
-    if (currentUser?.id) {
-      await handleUpdateUser();
-    } else {
-      await handleCreateUser();
-    }
-  };
-
-  // Create user
-  const handleCreateUser = async () => {
-    if (!currentUser) return;
-
-    try {
-      const res = await authFetch(`${VITE_API_URL}/api/users`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(currentUser),
-      });
-
-      const data = await res.json();
-
-      if (!res.ok) {
-        alert(data.message || t("users_screen.errors.create"));
-        return;
-      }
-
-      alert(t("users_screen.success.create"));
-      setOpenEdit(false);
-      setCurrentUser(null);
-      fetchUsers();
-    } catch (error) {
-      console.error(t("users_screen.errors.create"), error);
-    }
   };
 
   // Update user
@@ -206,8 +156,7 @@ export default function UsersTable() {
         />
 
         <Can permission="create_users">
-          <button className="px-4 py-2 bg-[#27B9BA] text-white rounded-lg shadow hover:bg-[#1da5a6] transition" 
-          onClick={handleOpenCreate}>
+          <button className="px-4 py-2 bg-[#27B9BA] text-white rounded-lg shadow hover:bg-[#1da5a6] transition" >
             {t("users_screen.add_button")}
           </button>
         </Can>
@@ -353,7 +302,7 @@ export default function UsersTable() {
               </button>
 
               <Can permission="update_users">
-                <button className="px-4 py-2 bg-[#27B9BA] text-white rounded-lg shadow hover:bg-[#1da5a6] transition" onClick={handleSaveUser}>
+                <button className="px-4 py-2 bg-[#27B9BA] text-white rounded-lg shadow hover:bg-[#1da5a6] transition" onClick={handleUpdateUser}>
                   {t("users_screen.modal.save")}
                 </button>
               </Can>
